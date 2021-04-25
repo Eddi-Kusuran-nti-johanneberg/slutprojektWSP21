f5e4b5c447eeaba088e912b67822048dafaa7a0f
Eddi-Kusuran-nti-johanneberg <eddi.kusuran@elev.ga.ntig.se>
2021-04-25 13:59:03 +0200

    Register
    Nu kan man registrera sig

--- app.rb
+++ app.rb
@@ -13,10 +13,14 @@ get('/showlogin') do
     slim(:login)
 end
 
+get('/characters') do
+    slim(:characters)
+end
+
 post('/login') do
     username = params[:username]
     password = params[:password]
-    db = SQLite3::Database.new('db/todo2021.db')
+    db = SQLite3::Database.new('db/slutprojektWSP21.db')
     db.results_as_hash = true
     result = db.execute("SELECT * FROM users WHERE username = ?",username).first
     pwdigest = result["pwdigest"]
@@ -28,4 +32,20 @@ post('/login') do
     else
       "FEL LÖSENORD!"
     end
+end
+
+post("/register") do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  if password == password_confirm
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new('db/slutprojektWSP21.db')
+    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+    redirect('/characters')
+
+  else
+    "Lösenorden matchade inte!"
+  end
 end
