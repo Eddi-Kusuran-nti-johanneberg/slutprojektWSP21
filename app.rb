7212c808e44643e84073dcbfb57ebc100621fdf9
Eddi-Kusuran-nti-johanneberg <eddi.kusuran@elev.ga.ntig.se>
2021-04-25 23:36:49 +0200

    Färdig (Nästan)
    Färdig för första inlämning

--- app.rb
+++ app.rb
@@ -18,7 +18,32 @@ get('/showlogin') do
 end
 
 get('/characters') do
-  slim(:characters)
+  id = session[:id].to_i
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM characters WHERE user_id = ?",id)
+  slim(:"characters",locals:{character:result})
+end
+
+get('/edit') do
+  slim(:edit)
+end
+
+post('/edit') do
+  id = session[:id].to_i
+  character = params[:character]
+  strength = params[:strength].to_i
+  intelligence = params[:intelligence].to_i
+  willpower = params[:willpower].to_i
+  agility = params[:agility].to_i
+  speed = params[:speed].to_i
+  endurance = params[:endurance].to_i
+  personality = params[:personality].to_i
+  luck = params[:luck].to_i
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  db.execute("INSERT INTO characters (name,strength,intelligence,willpower,agility,speed,endurance,personality,luck,user_id) VALUES (?,?,?,?,?,?,?,?,?,?)",character,strength,intelligence,willpower,agility,speed,endurance,personality,luck,id).first
+  redirect('/characters')
 end
 
 get('/items') do
@@ -36,6 +61,31 @@ post('/items') do
   db = SQLite3::Database.new('db/slutprojektWSP21.db')
   db.results_as_hash = true
   db.execute("INSERT INTO items (name,damage,user_id) VALUES (?,?,?)",item,damage,id).first
+  redirect('/items')
+end
+
+post('/items/:id/delete') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.execute("DELETE FROM items WHERE id = ?",id)
+  redirect("/items")
+end
+
+post('/items/:id/edit') do
+  id = params[:id].to_i
+  item = params[:item]
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  db.execute("UPDATE items SET name=? WHERE id=?",item,id).first
+  redirect('/items')
+end
+
+post('/items_damage/:id/edit') do
+  id = params[:id].to_i
+  damage = params[:item].to_i
+  db = SQLite3::Database.new('db/slutprojektWSP21.db')
+  db.results_as_hash = true
+  db.execute("UPDATE items SET damage=? WHERE id=?",damage,id).first
   redirect('/items')
 end
 
